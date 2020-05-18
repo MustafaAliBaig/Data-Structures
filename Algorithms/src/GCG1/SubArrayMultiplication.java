018fea0599893f8e5d00a22fa56d6f0262400c3e
mz564k <mz564k@CACDTL14JP925A.ITServices.sbc.com>
2020-05-18 10:32:59 -0700

    Modified the program but need to revisit the program as result is not as expected

--- Algorithms/src/GCG1/SubArrayMultiplication.java
+++ Algorithms/src/GCG1/SubArrayMultiplication.java
@@ -2,18 +2,22 @@ package GCG1;
 
 public class SubArrayMultiplication {
 	
+	
+	
+	//This program has to be revisited as result is not as expected
 	public static void SubArray_Multiply(int[] arr, int n)
 	{
 		int result=1;
-		/*
+
 		for(int i=0;i<n;i++)
 		{
+		int product=1;
 		for(int j=i;j<n;j++)
 			{
-			for(int k=i;k<=j;k++)
-			result *=arr[k];;
+			product=product*arr[j];
+			result *=product;
 			}
-		}*/
+		}
 		System.out.println(result +" \n");
 		
 	}
@@ -23,7 +27,6 @@ public class SubArrayMultiplication {
 		
 		int[] arr= {10,3,7};
 		int n=arr.length;
-		
 		SubArray_Multiply(arr, n);
 		
 
